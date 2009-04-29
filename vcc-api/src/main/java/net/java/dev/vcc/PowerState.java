018e86e677f31db4a3f5755ce9820debaceb0a65
stephenconnolly <stephenconnolly@0d01bcb8-1219-4a10-d7a5-d38bbd98f186>
2009-04-29 17:06:34 +0000

    OK, I've decided on the power states
    git-svn-id: https://svn.java.net/svn/vcc~svn/trunk@24 0d01bcb8-1219-4a10-d7a5-d38bbd98f186

--- vcc-api/src/main/java/net/java/dev/vcc/PowerState.java
+++ vcc-api/src/main/java/net/java/dev/vcc/PowerState.java
@@ -3,11 +3,28 @@ package net.java.dev.vcc;
 /**
  * Represents the power states of a virtual machine.
  */
-public enum PowerState {
-    POWERED_OFF,
+public enum PowerState
+{
+    /**
+     * The virtual machine has been powered off, to power it on again will
+     * require going through the BIOS boot sequence.
+     */
+    STOPPED,
+
+    /**
+     * A running virtual machine's state has been written out to disk, powering it
+     * on again will resume it from this state and will be delayed while the state
+     * is restored from disk.
+     */
     SUSPENDED,
+
+    /**
+     * Execution of a running virtual machine has been paused, the state is
+     * still in memory but it is not consuming CPU cycles.
+     */
     PAUSED,
-    POWERED_ON,
-    UNKNOWN,
-    TRANSITIONAL,
-}
+
+    /**
+     * The virtual machine's state is in memory and active.
+     */
+    RUNNING, }
