a971e36ed855f7476f99f1268ae2b0fafd860a59
stephenconnolly <stephenconnolly@0d01bcb8-1219-4a10-d7a5-d38bbd98f186>
2009-04-30 16:44:44 +0000

    adding some SPI classes
    git-svn-id: https://svn.java.net/svn/vcc~svn/trunk@28 0d01bcb8-1219-4a10-d7a5-d38bbd98f186

---
+++ vcc-api/src/main/java/net/java/dev/vcc/spi/AbstractHost.java
@@ -0,0 +1,13 @@
+package net.java.dev.vcc.spi;
+
+import net.java.dev.vcc.api.Host;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: user
+ * Date: 30-Apr-2009
+ * Time: 17:40:33
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class AbstractHost implements Host {
+}
