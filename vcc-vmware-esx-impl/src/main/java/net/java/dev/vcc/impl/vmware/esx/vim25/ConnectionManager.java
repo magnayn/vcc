ee3ee0146a2c8596438e8ed0df1ecb0e89b6df52
stephenconnolly <stephenconnolly@0d01bcb8-1219-4a10-d7a5-d38bbd98f186>
2009-05-05 09:17:58 +0000

    we have issues with accessing the WSDL from a SSL server using a self-signed key
    git-svn-id: https://svn.java.net/svn/vcc~svn/trunk@39 0d01bcb8-1219-4a10-d7a5-d38bbd98f186

--- vcc-vmware-esx-impl/src/main/java/net/java/dev/vcc/impl/vmware/esx/vim25/ConnectionManager.java
+++ vcc-vmware-esx-impl/src/main/java/net/java/dev/vcc/impl/vmware/esx/vim25/ConnectionManager.java
@@ -6,37 +6,23 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import javax.xml.namespace.QName;
 import javax.xml.ws.BindingProvider;
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.*;
 
 import com.vmware.vim25.VimPortType;
 import com.vmware.vim25.VimService;
 import com.vmware.vim25.ManagedObjectReference;
-import com.sun.xml.ws.developer.JAXWSProperties;
 
 public class ConnectionManager {
 
     private static final Logger LOGGER = Logger.getLogger(ConnectionManager.class.getName());
 
     public static VimPortType getConnection(String url) throws MalformedURLException {
-        final URL wsdlUrl = url.endsWith("/") ? new URL(url + "vimService.wsdl"): new URL(url + "/vimService.wsdl");
-        final VimPortType proxy =
-                new VimService(wsdlUrl, new QName("urn:vim25Service", "VimService")).getVimPort();
+        final VimPortType proxy = new VimService().getVimPort();
         final BindingProvider bindingProvider = (BindingProvider) proxy;
-        bindingProvider.getRequestContext()
-                .put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, url + "/vimService/");
+        bindingProvider.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+                url.endsWith("/") ? url + "vimService/" : url + "/vimService/");
         bindingProvider.getRequestContext().put(BindingProvider.SESSION_MAINTAIN_PROPERTY, true);
-        // Install the all-trusting hostname verifier
-        // VMware ESX servers typically do not have a cert with the correct hostname
-        bindingProvider.getRequestContext().put(JAXWSProperties.HOSTNAME_VERIFIER, new HostnameVerifier() {
-            public boolean verify(String s, SSLSession sslSession) {
-                System.out.println("verifying " + s);
-                return true;
-            }
-        });
+
         // Install the all-trusting trust manager
         // VMware ESX servers typically have a self-signed cert with the correct hostname
         try {
@@ -56,10 +42,28 @@ public class ConnectionManager {
                         }
                     }
             }, new java.security.SecureRandom());
-            bindingProvider.getRequestContext().put(JAXWSProperties.SSL_SOCKET_FACTORY, sc.getSocketFactory());
+            SSLSocketFactory sslSocketFactory = sc.getSocketFactory();
+            bindingProvider.getRequestContext().put("com.sun.xml.internal.ws.transport.https.client.SSLSocketFactory", sslSocketFactory);
+            bindingProvider.getRequestContext().put("com.sun.xml.ws.transport.https.client.SSLSocketFactory", sslSocketFactory);
+//            bindingProvider.getRequestContext().put(com.sun.xml.internal.ws.developer.JAXWSProperties.SSL_SOCKET_FACTORY, sslSocketFactory);
+//            bindingProvider.getRequestContext().put(com.sun.xml.ws.developer.JAXWSProperties.SSL_SOCKET_FACTORY, sslSocketFactory);
         } catch (Exception e) {
             LOGGER.log(Level.INFO, "Could not install the all-trusting socket factory", e);
         }
+
+        // Install the all-trusting hostname verifier
+        // VMware ESX servers typically do not have a cert with the correct hostname
+        final HostnameVerifier verifier = new HostnameVerifier() {
+            public boolean verify(String s, SSLSession sslSession) {
+                System.out.println("verifying " + s);
+                return true;
+            }
+        };
+
+        bindingProvider.getRequestContext().put("com.sun.xml.internal.ws.transport.https.client.hostname.verifier", verifier);
+        bindingProvider.getRequestContext().put("com.sun.xml.ws.transport.https.client.hostname.verifier", verifier);
+//        bindingProvider.getRequestContext().put(com.sun.xml.internal.ws.developer.JAXWSProperties.HOSTNAME_VERIFIER, verifier);
+//        bindingProvider.getRequestContext().put(com.sun.xml.ws.developer.JAXWSProperties.HOSTNAME_VERIFIER, verifier);
 
         return proxy;
     }
