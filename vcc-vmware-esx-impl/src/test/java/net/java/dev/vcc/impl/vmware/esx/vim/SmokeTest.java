169e3c06b7de11776bc4c957555c5341e6e66ad8
stephenconnolly <stephenconnolly@0d01bcb8-1219-4a10-d7a5-d38bbd98f186>
2009-06-30 15:44:52 +0000

    adding a basic test for the older webservice api
    git-svn-id: https://svn.java.net/svn/vcc~svn/trunk@223 0d01bcb8-1219-4a10-d7a5-d38bbd98f186

---
+++ vcc-vmware-esx-impl/src/test/java/net/java/dev/vcc/impl/vmware/esx/vim/SmokeTest.java
@@ -0,0 +1,85 @@
+package net.java.dev.vcc.impl.vmware.esx.vim;
+
+import com.vmware.vim.ManagedObjectReference;
+import com.vmware.vim.ObjectContent;
+import com.vmware.vim.PropertyFilterSpec;
+import com.vmware.vim.PropertySpec;
+import com.vmware.vim.ServiceContent;
+import com.vmware.vim.TraversalSpec;
+import com.vmware.vim.UserSession;
+import com.vmware.vim.VimPortType;
+import net.java.dev.vcc.impl.vmware.esx.JavaBeanHelper;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Basic tests
+ */
+public class SmokeTest {
+
+    private static final String URL = "http or https://vmware esx or vmware server hostname/sdk";
+
+    private static final String USERNAME = "please populate";
+
+    private static final String PASSWORD = "please populate";
+
+    @Ignore("Provide proper connection details")
+    @Test
+    public void smokeTest() throws Exception {
+
+        final VimPortType proxy = ConnectionManager.getConnection(URL);
+        final ManagedObjectReference serviceInstance = ConnectionManager.getServiceInstance();
+
+        ServiceContent serviceContent = proxy.retrieveServiceContent(serviceInstance);
+        ManagedObjectReference sessionManager = serviceContent.getSessionManager();
+        UserSession session = proxy.login(sessionManager, USERNAME, PASSWORD, null);
+        try {
+            JavaBeanHelper.describe(session);
+            JavaBeanHelper.describe(serviceContent);
+
+            TraversalSpec resourcePoolTraversalSpec = Helper
+                    .newTraversalSpec("resourcePoolTraversalSpec", "ResourcePool", "resourcePool", false,
+                            Helper.newSelectionSpec("resourcePoolTraversalSpec"));
+
+            TraversalSpec computeResourceRpTraversalSpec = Helper
+                    .newTraversalSpec("computeResourceRpTraversalSpec", "ComputeResource", "resourcePool", false,
+                            Helper.newSelectionSpec("resourcePoolTraversalSpec"));
+
+            TraversalSpec computeResourceHostTraversalSpec = Helper
+                    .newTraversalSpec("computeResourceHostTraversalSpec", "ComputeResource", "host", false);
+
+            TraversalSpec datacenterHostTraversalSpec = Helper
+                    .newTraversalSpec("datacenterHostTraversalSpec", "Datacenter", "hostFolder", false,
+                            Helper.newSelectionSpec("folderTraversalSpec"));
+
+            TraversalSpec datacenterVmTraversalSpec = Helper
+                    .newTraversalSpec("datacenterVmTraversalSpec", "Datacenter", "vmFolder", false,
+                            Helper.newSelectionSpec("folderTraversalSpec"));
+
+            TraversalSpec folderTraversalSpec = Helper
+                    .newTraversalSpec("folderTraversalSpec", "Folder", "childEntity", false,
+                            Helper.newSelectionSpec("folderTraversalSpec"),
+                            datacenterHostTraversalSpec,
+                            datacenterVmTraversalSpec,
+                            computeResourceRpTraversalSpec,
+                            computeResourceHostTraversalSpec,
+                            resourcePoolTraversalSpec);
+
+            PropertySpec meName = Helper.newPropertySpec("ManagedEntity", false, "name");
+
+            PropertyFilterSpec spec = Helper.newPropertyFilterSpec(meName,
+                    Helper.newObjectSpec(serviceContent.getRootFolder(), false, folderTraversalSpec));
+
+            for (ObjectContent c : proxy
+                    .retrieveProperties(serviceContent.getPropertyCollector(), Arrays.asList(spec))) {
+                JavaBeanHelper.describe(c);
+            }
+
+        } finally {
+            proxy.logout(sessionManager);
+        }
+    }
+
+}
