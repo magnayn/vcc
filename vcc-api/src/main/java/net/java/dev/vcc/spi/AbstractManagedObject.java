3548d532a5bb489b063f923050a37379d6fed925
stephenconnolly <stephenconnolly@0d01bcb8-1219-4a10-d7a5-d38bbd98f186>
2009-08-07 08:16:50 +0000

    ensure that managed objects have the correct equals and hashcode implementations
    git-svn-id: https://svn.java.net/svn/vcc~svn/trunk@277 0d01bcb8-1219-4a10-d7a5-d38bbd98f186

--- vcc-api/src/main/java/net/java/dev/vcc/spi/AbstractManagedObject.java
+++ vcc-api/src/main/java/net/java/dev/vcc/spi/AbstractManagedObject.java
@@ -4,20 +4,41 @@ import net.java.dev.vcc.api.ManagedObject;
 import net.java.dev.vcc.api.ManagedObjectId;
 
 /**
- * Created by IntelliJ IDEA.
- * User: connollys
- * Date: May 26, 2009
- * Time: 2:16:00 PM
- * To change this template use File | Settings | File Templates.
+ * Created by IntelliJ IDEA. User: connollys Date: May 26, 2009 Time: 2:16:00 PM To change this template use File |
+ * Settings | File Templates.
  */
 public abstract class AbstractManagedObject<T extends ManagedObject<T>> implements ManagedObject<T> {
     private final ManagedObjectId<T> id;
 
     protected AbstractManagedObject(ManagedObjectId<T> id) {
+        id.getClass(); // throw NPE if null
         this.id = id;
     }
 
     public ManagedObjectId<T> getId() {
         return id;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof AbstractManagedObject)) {
+            return false;
+        }
+
+        AbstractManagedObject that = (AbstractManagedObject) o;
+
+        if (!id.equals(that.id)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public final int hashCode() {
+        return id.hashCode();
     }
 }
