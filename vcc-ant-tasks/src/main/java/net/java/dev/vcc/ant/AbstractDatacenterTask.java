7ba1c702959c37fc4332daec381f92686751a0ff
stephenconnolly <stephenconnolly@0d01bcb8-1219-4a10-d7a5-d38bbd98f186>
2009-08-13 12:16:10 +0000

    [FIXED VCC-1]
    git-svn-id: https://svn.java.net/svn/vcc~svn/trunk@326 0d01bcb8-1219-4a10-d7a5-d38bbd98f186

--- vcc-ant-tasks/src/main/java/net/java/dev/vcc/ant/AbstractDatacenterTask.java
+++ vcc-ant-tasks/src/main/java/net/java/dev/vcc/ant/AbstractDatacenterTask.java
@@ -1,17 +1,20 @@
 package net.java.dev.vcc.ant;
 
-import org.apache.tools.ant.Task;
-import org.apache.tools.ant.BuildException;
 import net.java.dev.vcc.api.Datacenter;
 import net.java.dev.vcc.api.DatacenterManager;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Task;
 
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 /**
  * The base class for tasks that require a datacenter connection.
  */
 public abstract class AbstractDatacenterTask extends Task {
-    
+
     /**
      * The URI fo the datacenter.
      */
@@ -30,48 +33,78 @@ public abstract class AbstractDatacenterTask extends Task {
     @Override
     public final void execute() throws BuildException {
         AntLogFactory.setTask(this);
-        if ( datacenterUri == null )
-        {
-            throw new BuildException( "The datacenter URI must be specified the datacenteruri attribute" );
+        if (datacenterUri == null) {
+            throw new BuildException("The datacenter URI must be specified the datacenteruri attribute");
         }
-        if ( username == null )
-        {
-            throw new BuildException( "The username must be specified the username attribute" );
+        if (username == null) {
+            throw new BuildException("The username must be specified the username attribute");
         }
-        if ( password == null )
-        {
-            throw new BuildException( "The password must be specified the password attribute" );
-        }
-        Datacenter datacenter = null;
-        try
-        {
-            datacenter = DatacenterManager.getConnection( datacenterUri, username, password.toCharArray() );
-            if ( datacenter == null )
-            {
-                throw new BuildException( "Unknown datacenter URI" );
-            }
-            execute( datacenter );
-        }
-        catch ( IOException e )
-        {
-            throw new BuildException( e.getMessage(), e );
-        }
-        finally
-        {
-            if ( datacenter != null )
-            {
-                datacenter.close();
-            }
-            AntLogFactory.setTask( null );
+        if (password == null) {
+            throw new BuildException("The password must be specified the password attribute");
         }
 
+        ClassLoader loader = getClass().getClassLoader();
+
+        if (loader instanceof URLClassLoader) {
+            log("Using " + loader + " as classloader", Project.MSG_DEBUG);
+            for (URL url : ((URLClassLoader) loader).getURLs()) {
+                log("  " + url, Project.MSG_INFO);
+            }
+        } else {
+            log("Using " + loader + " as classloader", Project.MSG_DEBUG);
+        }
+
+        try {
+            ClassLoaderFixerThread fixer = new ClassLoaderFixerThread();
+            fixer.setContextClassLoader(loader);
+            fixer.start();
+            try {
+                fixer.join();
+            } catch (InterruptedException e) {
+                throw new BuildException(e.getMessage(), e);
+            }
+            if (fixer.datacenter == null) {
+                throw new BuildException("Unknown datacenter URI");
+            }
+            if (fixer.ioException != null) {
+                throw new BuildException(fixer.ioException.getMessage(), fixer.ioException);
+            }
+        } finally {
+            AntLogFactory.setTask(null);
+        }
+
+    }
+
+    private final class ClassLoaderFixerThread extends Thread {
+        private volatile Datacenter datacenter = null;
+        private volatile IOException ioException = null;
+
+        @Override
+        public void run() {
+            try {
+                datacenter = DatacenterManager
+                        .getConnection(datacenterUri, username, password.toCharArray());
+                if (datacenter == null) {
+                    return;
+                }
+                execute(datacenter);
+            } catch (IOException e) {
+                ioException = e;
+            } finally {
+                if (datacenter != null) {
+                    datacenter.close();
+                }
+                AntLogFactory.setTask(null);
+            }
+        }
     }
 
     /**
      * Execute the task on the specified datacenter.
      *
      * @param datacenter The datacenter connection.
-     * @throws BuildException   when things go bad.
+     *
+     * @throws BuildException when things go bad.
      */
     protected abstract void execute(Datacenter datacenter) throws BuildException;
 
